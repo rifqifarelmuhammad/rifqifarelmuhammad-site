a8995de28595f587a196b4a932b5474808d488f7
rifqifarelmuhammad <rifqifarel222@gmail.com>
2023-12-25 14:22:19 +0700

    prettier

--- src/components/elements/Footer/interface.ts
+++ src/components/elements/Footer/interface.ts
@@ -1,9 +1,9 @@
-import { IconType } from "react-icons"
+import { IconType } from 'react-icons'
 
 export type ContactType = 'LINKEDIN' | 'MAIL' | 'GITHUB'
 
 export interface ContactInterface {
-    type: ContactType
-    Icon: IconType
-    url: string
+  type: ContactType
+  Icon: IconType
+  url: string
 }
