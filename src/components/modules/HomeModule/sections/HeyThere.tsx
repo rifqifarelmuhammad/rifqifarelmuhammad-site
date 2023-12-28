7d696796fb85c8994c8016c67bb951f105c8fe26
rifqifarelmuhammad <rifqifarel222@gmail.com>
2023-12-28 17:54:33 +0700

    prettier

--- src/components/modules/HomeModule/sections/HeyThere.tsx
+++ src/components/modules/HomeModule/sections/HeyThere.tsx
@@ -6,7 +6,17 @@ export const HeyThere = () => (
     </h3>
 
     <h1 className="text-white text-xl">
-    I&rsquo;m Rifqi Farel Muhammad, a Computer Science student at University of Indonesia. I have a strong passion for software engineering, particularly in web development. I always push myself to improve my skills and learn new technologies. I have experience in Java, TypeScript, JavaScript, Python, Dart, and Golang, with a strong understanding of object-oriented programming. I have also implemented RESTful APIs, built interactive and responsive websites, built microservices architectures, and optimized the performance of the systems. I have worked on several projects, both on the backend and frontend. Creating high-quality products with efficient solutions and exceptional user experiences is what truly brings me fulfillment.
+      I&rsquo;m Rifqi Farel Muhammad, a Computer Science student at University
+      of Indonesia. I have a strong passion for software engineering,
+      particularly in web development. I always push myself to improve my skills
+      and learn new technologies. I have experience in Java, TypeScript,
+      JavaScript, Python, Dart, and Golang, with a strong understanding of
+      object-oriented programming. I have also implemented RESTful APIs, built
+      interactive and responsive websites, built microservices architectures,
+      and optimized the performance of the systems. I have worked on several
+      projects, both on the backend and frontend. Creating high-quality products
+      with efficient solutions and exceptional user experiences is what truly
+      brings me fulfillment.
     </h1>
   </div>
 )
