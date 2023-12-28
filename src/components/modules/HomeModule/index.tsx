a0dc077a5bd53ab665199ba1d364ac7758b012be
rifqifarelmuhammad <rifqifarel222@gmail.com>
2023-12-28 22:36:35 +0700

    improve: responsive

--- src/components/modules/HomeModule/index.tsx
+++ src/components/modules/HomeModule/index.tsx
@@ -1,8 +1,11 @@
+import { PageAnimation } from '@elements'
 import { HeyThere, LanguagesFrameworksTools } from './sections'
 
 export const HomeModule = () => (
-  <section className="max-w-[1440px] w-full mx-auto flex flex-col gap-16 md:gap-20 lg:gap-24 px-6 md:px-8 lg:px-12 xl:px-16 py-10 md:py-14 lg:py-20">
-    <HeyThere />
-    <LanguagesFrameworksTools />
-  </section>
+  <PageAnimation>
+    <section className="max-w-[1440px] w-full mx-auto flex flex-col gap-16 md:gap-20 lg:gap-24 px-6 md:px-8 lg:px-12 xl:px-16 py-10 md:py-14 lg:py-20">
+      <HeyThere />
+      <LanguagesFrameworksTools />
+    </section>
+  </PageAnimation>
 )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { HomePageAnimation } from '@elements'
 import { useEffect, useState } from 'react'
-import { motion } from 'framer-motion'
 import { HomeModule } from '@modules'
 
 export default function Home() {
@@ -9,7 +8,7 @@ export default function Home() {
   useEffect(() => {
     setTimeout(() => {
       setAnimation(false)
-    }, 2950)
+    }, 3000)
   }, [])
 
   return (
@@ -19,12 +18,7 @@ export default function Home() {
           <HomePageAnimation />
         ) : (
           <>
-            <motion.div
-              animate={{ scale: [0, 0.25, 0.5, 0.75, 1] }}
-              transition={{ times: 1 }}
-            >
-              <HomeModule />
-            </motion.div>
+            <HomeModule />
           </>
         )}
       </div>
