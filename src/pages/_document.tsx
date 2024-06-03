aefa3d9a8adc0d09dffa9b2f86b3c81d32f0fef5
rifqifarelmuhammad <rifqifarel222@gmail.com>
2024-06-04 00:30:46 +0700

    refactor(head): improved head to be more semantically

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,25 +3,26 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth bg-[#0B1726]">
-      <Head />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/images/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/images/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/images/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/site.webmanifest" />
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body>
         <Main />
         <NextScript />
