78595bcbca8285e8e63bfb70c9abeb346144e139
rifqifarelmuhammad <rifqifarel222@gmail.com>
2024-06-05 01:15:53 +0700

    refactor(component): prettier

--- src/components/elements/PageAnimation/index.tsx
+++ src/components/elements/PageAnimation/index.tsx
@@ -2,7 +2,11 @@ import { motion } from 'framer-motion'
 import { VARIANTS } from './constant'
 import { ReactNode } from 'react'
 
-export const PageAnimation = ({ children }: { children: ReactNode }): JSX.Element => (
+export const PageAnimation = ({
+  children,
+}: {
+  children: ReactNode
+}): JSX.Element => (
   <motion.div initial="hidden" animate="enter" exit="exit" variants={VARIANTS}>
     {children}
   </motion.div>
